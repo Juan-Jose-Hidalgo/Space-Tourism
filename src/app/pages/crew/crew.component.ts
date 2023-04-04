e9a5afd7305dcd37cf7dc27024558093a4fd4cfd
Juan Hidalgo <juanhidalgoyanez@gmail.com>
2023-04-04 11:34:57 +0200

    feature(crew): Add crew and crew-info components
    - Create Crew component for displaying information about crew members.
    - Create Crew-Info component for displaying specific details about a crew member within Crew component.

--- src/app/pages/crew/crew.component.ts
+++ src/app/pages/crew/crew.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, HostListener, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { CrewMember } from 'src/app/models/crew-member.interface';
 import { CrewService } from 'src/app/services/crew.service';
@@ -10,8 +10,47 @@ import { CrewService } from 'src/app/services/crew.service';
 })
 export class CrewComponent implements OnInit, OnDestroy {
 
+  public selectedIndex = 0;
   public crew: CrewMember[] = [];
+
   private crewSub!: Subscription;
+  private touchEndX!: number;
+  private touchStarted = false;
+  private touchStartX!: number;
+
+  @HostListener('touchstart', ['$event'])
+  onTouchStart(event: TouchEvent): void {
+    this.touchStartX = event.touches[0].clientX;
+    this.touchStarted = true;
+  }
+
+  @HostListener('touchmove', ['$event'])
+  onTouchMove(event: TouchEvent): void {
+    if (!this.touchStarted) return;
+    this.touchEndX = event.touches[0].clientX;
+  }
+
+  @HostListener('touchmove', ['$event'])
+  ontTouchMove(event: TouchEvent): void {
+    if (!this.touchStarted) return;
+    this.touchEndX = event.touches[0].clientX;
+  }
+
+  @HostListener('touchend')
+  onTouchEnd(): void {
+    if (!this.touchStarted) return;
+
+    const touchDiff = this.touchStartX - this.touchEndX;
+    const absTouchDiff = Math.abs(touchDiff);
+
+    // Change slide if swipe distance is greater than 50px
+    if (absTouchDiff > 50) {
+      if (touchDiff < 0) this.selectNext();
+      else this.selectPrevious();
+    }
+
+    this.touchStarted = false;
+  }
 
   constructor(
     private cs: CrewService
@@ -21,12 +60,24 @@ export class CrewComponent implements OnInit, OnDestroy {
     this.getCrew();
   }
 
+  ngOnDestroy(): void {
+    this.crewSub.unsubscribe();
+  }
+
   getCrew() {
     this.crewSub = this.cs.getCrew().subscribe(crew => this.crew = crew);
   }
 
-  ngOnDestroy(): void {
-    this.crewSub.unsubscribe();
+  selectItem(index: number) {
+    this.selectedIndex = index;
+  }
+
+  selectNext(): void {
+    this.selectedIndex = (this.selectedIndex === this.crew.length - 1) ? 0 : this.selectedIndex + 1;
+  }
+
+  selectPrevious(): void {
+    this.selectedIndex = (this.selectedIndex === 0) ? this.crew.length - 1 : this.selectedIndex - 1;
   }
 
 }
