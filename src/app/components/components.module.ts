b574eb3180a3ac9d218f30a778bcd00a8c000351
Juan Hidalgo <juanhidalgoyanez@gmail.com>
2023-04-07 10:31:10 +0200

    refactor(crew): ♻️ Change styles, move components, delete title.service
    - Modify styles to maintain consistent design with the rest of the pages.
    - Create a components folder in crew module.
    - Move crew-info component from components module to /crew/components.
    - Delete title.service.

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
-import { CrewInfoComponent } from './crew-info/crew-info.component';
 import { TechInfoComponent } from './tech-info/tech-info.component';
 import { TechNavComponent } from './tech-nav/tech-nav.component';
 
@@ -10,12 +9,10 @@ import { TechNavComponent } from './tech-nav/tech-nav.component';
 
 @NgModule({
   declarations: [
-    CrewInfoComponent,
     TechInfoComponent,
     TechNavComponent,
   ],
   exports: [
-    CrewInfoComponent,
     TechInfoComponent,
     TechNavComponent
   ],
--- src/app/pages/crew/crew.module.ts
+++ src/app/pages/crew/crew.module.ts
@@ -3,23 +3,23 @@ import { CommonModule } from '@angular/common';
 
 import { NgOptimizedImage } from '@angular/common'
 
-import { ComponentsModule } from 'src/app/components/components.module';
 import { SharedModule } from 'src/app/shared/shared.module';
 
 import { CrewRoutingModule } from './crew-routing.module';
 import { CrewComponent } from './crew.component';
+import { CrewInfoComponent } from './components/crew-info/crew-info.component';
 
 
 @NgModule({
   declarations: [
-    CrewComponent
+    CrewComponent,
+    CrewInfoComponent
   ],
   exports: [
     CrewComponent
   ],
   imports: [
     CommonModule,
-    ComponentsModule,
     CrewRoutingModule,
     NgOptimizedImage,
     SharedModule
